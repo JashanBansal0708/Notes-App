642cdc6ebcfba500b1b3d2e04e5cc13e9fe0123d
JashanBansal0708 <jashanbansal7856@yahoo.com>
2018-09-23 20:24:28 +0530

    Add the --help functionality at command Line.

--- app.js
+++ app.js
@@ -6,9 +6,34 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+var titleOptions = {
+    describe : 'Title of note',
+    demand : true,
+    alias : 't'
+} 
 
-const argv = yargs.argv;
-var command = argv._[0];
+var bodyOptions = {
+    describe : 'Body of note',
+    demand : true,
+    alias : 'b'
+}
+
+const argv = yargs
+    .command('add', 'Add a new note', {            // Options object
+        title : titleOptions,
+        body : bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title : titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title : titleOptions
+    })
+    .help()
+    .argv;
+
+    var command = argv._[0];
 // console.log('Command: ', command);
 // console.log('Yargs', argv);
 
